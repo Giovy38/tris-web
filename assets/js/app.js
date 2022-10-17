357670bf14c497ed38aae49dfa61afbf6f067621
Giovanni <giovidecaro@live.it>
2022-10-17 18:44:04 +0200

    insert symbol

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,50 @@
 class Symbol{
-    constructor(){
-        this.symbol = 'X';
-    }
-
-    changeSymbol(newSymbol){
-        if(this.symbol === 'X'){
-            newSymbol = 'O'
-        }else{
-            newSymbol = 'X'
-        }
-        console.log(this.symbol);
-        return newSymbol;
+    constructor(value){
+        this.value = value;
     }
 }
+// add symbol function
+const addSymbol = async (id) => {
+    const symboltras = await document.getElementById(`${id}-h1`);
+    await symboltras.classList.remove('trasparent')
+};
 
-document.getElementById('zero').addEventListener('click',insertValue);
-document.getElementById('one').addEventListener('click',insertValue);
-document.getElementById('two').addEventListener('click',insertValue);
-document.getElementById('three').addEventListener('click',insertValue);
-document.getElementById('four').addEventListener('click',insertValue);
-document.getElementById('five').addEventListener('click',insertValue);
-document.getElementById('six').addEventListener('click',insertValue);
-document.getElementById('seven').addEventListener('click',insertValue);
-document.getElementById('eight').addEventListener('click',insertValue);
+// zero 
+const zero = document.getElementById('zero');
+const callZeroSymbol = () => addSymbol('zero');
+zero.addEventListener('click', callZeroSymbol);
 
-function insertValue(){
-    const symbol = new Symbol();
-    
 
-    document.querySelectorAll('.symbol').forEach(symbol => {
-        const reachSymbol = symbol.textContent;
-        symbol.changeSymbol(reachSymbol);
-        console.log('foreach:0',symbol.textContent);
-        console.log('this',this.symbol);
-    })
+// one
+const one = document.getElementById('one');
+const callOneSymbol = () => addSymbol('one');
+one.addEventListener('click', callOneSymbol);
+// two
+const two = document.getElementById('two');
+const callTwoSymbol = () => addSymbol('two');
+two.addEventListener('click', callTwoSymbol);
+// three
+const three = document.getElementById('three');
+const callThreeSymbol = () => addSymbol('three');
+three.addEventListener('click', callThreeSymbol);
+// four
+const four = document.getElementById('four');
+const callFourSymbol = () => addSymbol('four');
+four.addEventListener('click', callFourSymbol);
+// five
+const five = document.getElementById('five');
+const callFiveSymbol = () => addSymbol('five');
+five.addEventListener('click', callFiveSymbol);
+// six
+const six = document.getElementById('six');
+const callSixSymbol = () => addSymbol('six');
+six.addEventListener('click', callSixSymbol);
+// seven
+const seven = document.getElementById('seven');
+const callSevenSymbol = () => addSymbol('seven');
+seven.addEventListener('click', callSevenSymbol);
+// eight
+const eight = document.getElementById('eight');
+const callEightSymbol = () => addSymbol('eight');
+eight.addEventListener('click', callEightSymbol);
 
-   
-}
